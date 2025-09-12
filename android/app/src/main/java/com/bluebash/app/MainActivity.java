eee249be2d061df4a3947bd964912494fb9ab100
luqman-bashir <uthamin98@gmail.com>
2025-09-12 15:13:44 +0000

    Capacitor Android setup: config, resources, generated assets, android project

---
+++ android/app/src/main/java/com/bluebash/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bluebash.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bluebash.app',
+  appName: 'Salam',
+  webDir: 'dist'
+};
+
+export default config;
